f8068633777f04e50af3eaf8563c37d5792a81b3
Priscilla Vazquez <scillamae922@gmail.com>
2019-07-27 18:54:02 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,17 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+ttt_board = ["","X"," "," "," "," "," "," "," "]
+puts "Enter a position on the board 1-9:"
+user_input = gets.chomp
+number_entered = user_input.to_i - 1
+
+def valid_move?(number_entered, board)
+  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
+end
+
+def position_taken?(board, index)
+ !(board[index] == " " || board[index] == "" || board[index] == nil)
+end
+
+valid_move?(number_entered, ttt_board)
